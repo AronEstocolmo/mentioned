c127280c68ee32b00da3da70ccba94d9f91680c5
Aron Sharma <aron.sharma@shibsted.se>
2018-01-16 10:42:45 +0100

    NYT working. Promise.all implemented to handle both API calls

--- src/ArticlesList.js
+++ src/ArticlesList.js
@@ -12,43 +12,25 @@ class ArticlesList extends Component {
  submit = () => {
   console.log(this.text.value)  
     const res = fetch(`https://content.guardianapis.com/search?api-key=d1358fca-f887-48cc-a9d8-5dee7e0ff85a&q=${this.text.value}&section=world`);
-    const res2 = fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=cff1cc49cb2f40ab97411b04432fcb72&q=${this.text.value}`);
+    const res2 = fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=cff1cc49cb2f40ab97411b04432fcb72&q=${this.text.value}&begin_date=19990101`);
     
-    res.then(function(result) {return result.json()}).then(res => this.setState({ articles: res.response }));
-    res2.then(function(result) {return result.json()}).then(res2 => this.setState({ nytArticles: res2 }));
-    
-    //res2.then(function(result) {return result.json()}).then((function(data) {console.log(data.response);}));
+    Promise.all([res, res2])
+    .then(data => {return Promise.all(data.map(r => r.json()));})
+    .then(data => this.setState({articles: data[0].response, nytArticles: data[1].response.meta}));
 
+    //res.then(function(result) {return result.json()}).then(res => this.setState({ articles: res.response }));
+    //res2.then(function(result) {return result.json()}).then(res2 => this.setState({ nytArticles: res2 }));
   }
-  /*  
-  async componentDidMount(){
-    try{
-      const res = await fetch('https://content.guardianapis.com/search?api-key=d1358fca-f887-48cc-a9d8-5dee7e0ff85a&q=egypt&section=world');
-      const articles = await res.json();
-      console.log("Lol");
-      console.log(articles.response.total);
-      console.log(articles);  
-
-      this.setState({
-        articles: articles.response
-      })
-
-    } catch(e){
-      console.log(e);
-    }
-  }
-  */
 
   render() {
     console.log(this.state.articles);
     console.log(this.state.nytArticles);
-    console.log("Shuuu");
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Mentions of your search term:</h1>
-          The Guardian: {this.state.articles.total}, The New York Times:
+          The Guardian: {this.state.articles.total} , The New York Times: {this.state.nytArticles.hits} 
         </header>
         <input type="text" ref={(input) => this.text=input}></input>
         <button onClick={this.submit}>Search</button>
